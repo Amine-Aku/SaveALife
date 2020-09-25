11d812344bf9c308c30e05bcbd0fd77ae685aa69
Amine-Aku <codenameaku@gmail.com>
2020-09-25 18:38:37 +0100

    + Firebase Cloud messaging : 1st Push Notification Test

---
+++ app/src/main/java/com/impression/savealife/api/FirebaseMsgService.kt
@@ -0,0 +1,68 @@
+package com.impression.savealife.api
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.impression.savealife.R
+import com.impression.savealife.activities.MainActivity
+
+open class FirebaseMsgService() : FirebaseMessagingService() {
+
+    private val TAG = "FirebaseMsgService"
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        if(remoteMessage!!.data.isNotEmpty())
+            Log.d(TAG, "onMessageReceived: msg data payload :  ${remoteMessage.data}")
+        remoteMessage!!.notification?.let {
+            val title = it.title
+            val body = it.body
+            Log.d(TAG, "onMessageReceived: Title : $title\nBody : $body")
+            sendNotification(title!!, body!!)
+        }
+    }
+
+    private fun sendNotification(title: String, messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = "New Post Notification"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.logo_save_a_life)
+            .setContentTitle(title)
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+
+    override fun onDeletedMessages() {
+        super.onDeletedMessages()
+    }
+
+    override fun onNewToken(token: String?) {
+        Log.d(TAG, "onNewToken: $token")
+    }
+}
